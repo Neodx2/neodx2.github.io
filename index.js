70d02ea93acf04383d5a0110e32536d178441278
Neo <119887617+NeoDarkless@users.noreply.github.com>
2023-12-20 08:45:52 +0000

    abv

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var time = new Date();
-
 function update_time() {
-    document.getElementById("hours").textContent=time.getHours();
-    document.getElementById("minutes").textContent=time.getMinutes();
-    document.getElementById("seconds").textContent=time.getSeconds();
+    var time = new Date();
+
+    document.getElementById("hours").textContent = time.getHours();
+    document.getElementById("minutes").textContent = time.getMinutes();
+    document.getElementById("seconds").textContent = time.getSeconds();
 }
